51306f2d58e4432733c516eb72865f663eef32b6
Chivo Coder <rodrigo.m.quintero@gmail.com>
2025-04-01 15:05:16 -0300

    first commit 🚧

--- hud_spartan_flutter/lib/theme/spartan_theme.dart
+++ hud_spartan_flutter/lib/theme/spartan_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class SpartanColors {
   static const background = Colors.black;
-  static const defaultText = Color(0xFF00FFCC); // Cian verdoso tipo ONI
+  static const defaultText = Color(0xFF00FFCC);
   static const success = Colors.greenAccent;
   static const warning = Colors.amberAccent;
   static const error = Colors.redAccent;
